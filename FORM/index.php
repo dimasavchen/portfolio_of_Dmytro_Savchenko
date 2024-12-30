89b728e5640e23687bd0a48d8c0a4e7ad9373551
dimasavchen <dimasavchenko201355@gmail.com>
2024-12-30 16:21:26 +0200

    test
    test

---
+++ FORM/index.php
@@ -0,0 +1,10 @@
+<!-- форма зворотнього зв'язку -->
+ <form action="" metod="">
+
+<input type="text" name="login" palceholder=" Введіть логін"/>
+<input type="email" name="email" palceholder=" Введіть пошту"/>
+<input type="tel" name="tel" palceholder=" Введіть телефон" />
+
+<input type="submit" value="Це відправити" />
+
+ </form>
